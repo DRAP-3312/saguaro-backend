0845b9a7030cc15d35fff10510dd3f3170497e07
DRAP-3312 <davidake117@gmail.com>
2024-12-01 21:36:27 -0500

    config swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,37 +1,96 @@
-import { IsEmail, IsIn, IsOptional, IsString, Length, MaxLength, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsIn,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @IsString()
-    @MinLength(2)
-    name:string
+  @ApiProperty({
+    description: 'name of user',
+    nullable: false,
+    minLength: 2,
+    maxLength: 50,
+    example: 'paco beto',
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  name: string;
 
-    @IsString()
-    @MinLength(2)
-    lastname:string
+  @ApiProperty({
+    description: 'lastname of user',
+    nullable: false,
+    minLength: 2,
+    maxLength: 50,
+    example: 'ayala chan',
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  lastname: string;
 
-    @IsString()
-    @IsEmail()
-    email:string
+  @ApiProperty({
+    description: 'email of user',
+    nullable: false,
+    example: 'pacob@google.com',
+  })
+  @IsString()
+  @IsEmail()
+  email: string;
 
-    @IsString()
-    @IsOptional()
-    @MaxLength(250)
-    description:string
+  @ApiProperty({
+    description: 'description of user',
+    nullable: true,
+    maxLength: 250,
+    example: 'me gustan los video juegos y deportes',
+  })
+  @IsString()
+  @IsOptional()
+  @MaxLength(250)
+  description: string;
 
-    @IsString()
-    @IsOptional()
-    picture:string
+  @ApiProperty({
+    description: 'picture of user',
+    nullable: true,
+    example: 'picture.jpg',
+  })
+  @IsString()
+  @IsOptional()
+  picture: string;
 
-    @IsIn(['male','female','notSay'])
-    gender:string
+  @ApiProperty({
+    description: 'gender of user',
+    nullable: false,
+    example: 'male',
+  })
+  @IsIn(['male', 'female', 'notSay'])
+  gender: string;
 
-    @IsString()
-    @MaxLength(10)
-    @MinLength(4)
-    phone:string
+  @ApiProperty({
+    description: 'phone of user',
+    nullable: false,
+    minLength: 4,
+    maxLength: 10,
+    example: '9977553322',
+  })
+  @IsString()
+  @MaxLength(10)
+  @MinLength(4)
+  phone: string;
 
-    @IsString()
-    @MinLength(2)
-    @MaxLength(4)
-    lada:string
+  @ApiProperty({
+    description: 'lada of user',
+    nullable: false,
+    minLength: 2,
+    maxLength: 4,
+    example: '+52',
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(4)
+  lada: string;
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,65 +1,73 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @ApiProperty({
-        example: '123e4567-e89b-12d3-a456-426655440000',
-        description: 'User ID',
-        uniqueItems: true
-    })
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+  @ApiProperty({
+    example: 'e1054272-7da4-4527-9358-662fd0552d5e',
+    description: 'User ID',
+    uniqueItems: true,
+  })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @ApiProperty({
-        example: 'Jose Marco',
-        description: 'User name',
-    })
-    @Column('text')
-    name: string
+  @ApiProperty({
+    example: 'Jose Marco',
+    description: 'User name',
+  })
+  @Column('text')
+  name: string;
 
-    @ApiProperty({
-        example: 'Chan Moines',
-        description: 'User lastname',
-    })
-    @Column('text')
-    lastname: string
+  @ApiProperty({
+    example: 'Chan Moines',
+    description: 'User lastname',
+  })
+  @Column('text')
+  lastname: string;
 
-    @ApiProperty({
-        example: 'jose@gmail.com',
-        description: 'User email',
-        uniqueItems: true
-    })
-    @Column('text')
-    email: string
+  @ApiProperty({
+    example: 'jose@gmail.com',
+    description: 'User email',
+    uniqueItems: true,
+  })
+  @Column('text')
+  email: string;
 
-    @ApiProperty({
-        example: 'megustan los video juegos',
-        description: 'User description',
-    })
-    @Column('text')
-    description: string
+  @ApiProperty({
+    example: 'me gustan los video juegos',
+    description: 'User description',
+  })
+  @Column('text')
+  description: string;
 
-    @ApiProperty({
-        example: 'mifoto.jpg',
-        description: 'User picture',
-        uniqueItems: true
-    })
-    @Column('text')
-    picture: string
+  @ApiProperty({
+    example: 'mifoto.jpg',
+    description: 'User picture',
+    uniqueItems: true,
+  })
+  @Column('text')
+  picture: string;
 
-    @ApiProperty({
-        example: 'male',
-        description: 'User gender',
-    })
-    @Column('text')
-    gender: string
+  @ApiProperty({
+    example: 'male',
+    description: 'User gender',
+  })
+  @Column('text')
+  gender: string;
 
-    @ApiProperty({
-        example: '4266554400',
-        description: 'User phone',
-        uniqueItems: true
-    })
-    @Column('text')
-    phone: string
+  @ApiProperty({
+    example: '4266554400',
+    description: 'User phone',
+    uniqueItems: true,
+  })
+  @Column('text')
+  phone: string;
+
+  @ApiProperty({
+    example: '+52',
+    description: 'User lada',
+    uniqueItems: false,
+  })
+  @Column('text')
+  lada: string;
 }
